e8449a1950fa60f7b1b13613a5ac1d9f1adb0552
Sarah-Bue Bue <buettner-sarah@gmx.net>
2024-05-27 09:51:42 +0000

    remove buttons, call screen when computer wins

--- test.js
+++ test.js
@@ -14,6 +14,7 @@ let mainLoginScreen = document.getElementById("login-screen");
 let getInstructions = document.getElementById("instructions-icon");
 let errorMessage = document.getElementById("error-message");
 let mainGameScreen = document.getElementById("main-game-container");
+let gameOverScreen = document.getElementById("game-over-screen");
 let modal = document.getElementById("myModal");
 let closeBtn = document.getElementById("close-btn");
 let closeXBtn = document.getElementById("close-x-btn");
@@ -27,6 +28,7 @@ function runMainScreen() {
     errorMessage.style.display = "none";
     mainGameScreen.style.display = "none";
     document.getElementById("user-icon").style.display = "none";
+    gameOverScreen.style.display = "none";
     document.getElementById("username").innerText = "";
     document.getElementById("user").focus(); //focus on input element with cursor ready for username input
 }
@@ -346,6 +348,10 @@ function checkGameEnd() {
             computerGameWins++;
             result = "computer wins"; // Assign the result
             console.log(result); // Log the result variable
+
+            // Load Game Over screen when compuer wins
+            gameOverScreen.style.display = "flex";
+
         } else {
             playerGameWins++;
             result = "player wins"; // Assign the result
